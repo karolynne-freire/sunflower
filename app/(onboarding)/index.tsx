6f8aef5af57fe555f3401fcdffa94313bb23f7e9
karolynne-freire <freirekarolynne@gmail.com>
2025-10-30 19:25:50 -0300

    feat: incluindo as telas de apresentação e login

--- app/(onboarding)/index.tsx
+++ app/(onboarding)/index.tsx
@@ -1,11 +1,37 @@
-import { Text, View } from "react-native";
-import PerguntaEmocional from "../../components/pergunta";
+import { Link } from "expo-router";
+import React from "react";
+import { Image, StyleSheet, Text, View } from "react-native";
+import Button from "../../components/button";
 
-export default function HomeScreen() {
+export default function Presentation1() {
   return (
-    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      <Text>🌻 Bem-vindo ao Sunflower! 🌻</Text>
-      <PerguntaEmocional />
+    <View style={styles.container}>
+      <Image
+        source={require("../../assets/img/feliz.png")}
+        style={styles.image}
+        resizeMode="contain"
+      />
+
+      <Text style={styles.title}>Olá, amiguinho(a)!</Text>
+      <Text style={styles.subtitle}>Eu sou o Sunny!</Text>
+
+      <Text style={styles.text}>
+        Aqui, nós vamos brincar e aprender juntos!{"\n"}
+        Durante o jogo, eu vou te fazer algumas perguntinhas.
+      </Text>
+
+<Link href="/presentation2" asChild>
+  <Button title="Avançar" />
+</Link>
+
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: { flex: 1, alignItems: "center", justifyContent: "center", backgroundColor: "#FAF8F0", padding: 20 },
+  image: { width: 180, height: 180, marginBottom: 20 },
+  title: { fontSize: 26, fontWeight: "bold", color: "#333" },
+  subtitle: { fontSize: 22, marginBottom: 10, color: "#333" },
+  text: { textAlign: "center", fontSize: 16, color: "#333", marginBottom: 30 },
+});
---
+++ components/button.tsx
@@ -0,0 +1,41 @@
+import React from "react";
+import { StyleSheet, Text, TouchableOpacity, ViewStyle } from "react-native";
+
+interface ButtonProps {
+  title: string;
+  onPress?: () => void;
+  backgroundColor?: string;
+  style?: ViewStyle;
+}
+
+export default function Button({
+  title,
+  onPress,
+  backgroundColor = "#AEE3F0",
+  style,
+}: ButtonProps) {
+  return (
+    <TouchableOpacity
+      style={[styles.button, { backgroundColor }, style]}
+      onPress={onPress}
+      activeOpacity={0.8}
+    >
+      <Text style={styles.text}>{title}</Text>
+    </TouchableOpacity>
+  );
+}
+
+const styles = StyleSheet.create({
+  button: {
+    paddingVertical: 12,
+    paddingHorizontal: 40,
+    borderRadius: 10,
+    alignItems: "center",
+    marginVertical: 5,
+  },
+  text: {
+    color: "#333",
+    fontWeight: "bold",
+    fontSize: 18,
+  },
+});
